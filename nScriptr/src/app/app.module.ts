feb7f5f46f8b45867915738a6a74fd5c34bb994c
Anna Westfall <anna.westfall@ncino.com>
2019-10-25 16:15:17 -0400

    in progress

--- nScriptr/src/app/app.module.ts
+++ nScriptr/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { ColorPickerModule } from 'ngx-color-picker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PopupComponent } from './popup/popup.component';
+import { OptionsPanelComponent } from './options-panel/options-panel.component';
+import { ResultsPanelComponent } from './results-panel/results-panel.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopupComponent,
+    OptionsPanelComponent,
+    ResultsPanelComponent
   ],
   imports: [
     BrowserModule,
 	 AppRoutingModule,
-	 ColorPickerModule
+	 ColorPickerModule,
+	 FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ nScriptr/src/app/results-panel/results-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-results-panel',
+  templateUrl: './results-panel.component.html',
+  styleUrls: ['./results-panel.component.css']
+})
+export class ResultsPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
